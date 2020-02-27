2abac85c3d9647ca464ddf9d70b7350b2dcfa323
Polina <ipolina33@gmail.com>
2020-02-27 15:46:17 +0500

    finished switching from horizontal to vertical view, the handle jumps to the entered value

--- src/index.js
+++ src/index.js
@@ -62,7 +62,6 @@ function addSliderWithControl (id, min, max) {
   let valueCheckbox = createElement("input", "slider__valueCheckbox", id);
   valueCheckbox.type = "checkbox";
   let checkboxLabel = createElement("label", "slider__valueCheckboxLabel", id);
-  // checkboxLabel.for = valueCheckbox.id;
   checkboxLabel.setAttribute('for', '' + valueCheckbox.id);
   checkboxLabel.innerHTML = "Показать значение над ползунком";
 
@@ -91,7 +90,6 @@ function addSliderWithControl (id, min, max) {
   viewRadioVerticalLabel.setAttribute("for", "" + viewRadioVertical.id);
   viewRadioVerticalLabel.innerHTML = "Вертикальный";
 
-
 //-----------------------------------------------------------------------------
 
   let addHandleButton = createElement("button", "slider__addHandleButton", id);
@@ -116,7 +114,6 @@ function addSliderWithControl (id, min, max) {
   $('#' + viewRadioContainer.id).append(viewRadioHorizontalLabel);
   $('#' + viewRadioContainer.id).append(viewRadioVertical);
   $('#' + viewRadioContainer.id).append(viewRadioVerticalLabel);
-  
 
   $('#' + controlElements.id).append(addHandleButton);
   $('#' + controlElements.id).append(deleteAllHandlesButton);
@@ -174,6 +171,9 @@ function rotateSliderVertical (containerId) {
   
     for (let handle of handles) {
       handle.classList.add("vertical");
+      let oldLeft = handle.style.left;
+      handle.style.left = -4 + "px";
+      handle.style.top = oldLeft;
       let handleId = "#" + handle.id;
       $(handleId).children(".slider__handleLabel").addClass("vertical");
     }
@@ -194,6 +194,9 @@ function rotateSliderHorisontal (containerId) {
 
     for (let handle of handles) {
       handle.classList.remove("vertical");
+      let oldTop = handle.style.top;
+      handle.style.top = -5 + "px";
+      handle.style.left = oldTop;
       let handleId = "#" + handle.id;
       $(handleId).children(".slider__handleLabel").removeClass("vertical");
     }
@@ -214,6 +217,11 @@ function addSliderHandle (id, max) {
   $(sliderScaleId).append(sliderHandle);
   $('#' + sliderHandle.id).append(sliderHandleLabel);
   startValueHint(sliderHandleLabel.id);
+
+  if (document.querySelector(sliderScaleId).classList.contains("vertical")){
+    sliderHandle.classList.add("vertical");
+    sliderHandleLabel.classList.add("vertical");
+  }
 
   document.querySelector("#" + sliderHandleLabel.id).innerHTML = max/2;
   letHandleRun();
@@ -245,6 +253,50 @@ function addHandleControl (handleId) {
 
   $("#" + handleControl.id).append(handleValueFieldLabel);
   $("#" + handleControl.id).append(handleValueField);
+
+  let max, min;
+
+  for (let j = 0; j < sliders.length; j++) {
+    if (sliders[j].id == "slider__container-" + idNumber ) {
+      max = sliders[j].maxScaleValue;
+      min = sliders[j].minScaleValue;
+    }
+  }
+
+  handleValueField.onchange = function() {
+    let newValue;
+    let inputValue = +handleValueField.value;
+
+    if (!isNaN(inputValue)) {
+
+      if (inputValue < min) {
+        inputValue = min;
+      }
+
+      if (inputValue > max) {
+        inputValue = max;
+      }
+
+
+      let handleId = "#slider__handle-" + idPostfix;
+      let sliderScaleId = "#slider__scale-" + idNumber;
+      let handleLabelId = "#slider__handleLabel-" + idPostfix;
+
+      let handle = document.querySelector(handleId);
+      let sliderScale = document.querySelector(sliderScaleId); 
+      let handleLabel = document.querySelector(handleLabelId);
+
+      if (handle.classList.contains("vertical")) {
+        newValue = ((sliderScale.getBoundingClientRect().height * (inputValue))/max).toFixed(0);
+        handle.style.top = newValue + "px";
+      } else {
+        newValue = ((sliderScale.getBoundingClientRect().width * inputValue)/max).toFixed(0);
+        handle.style.left = newValue + "px";
+      }
+
+      handleLabel.innerHTML = inputValue;
+    }
+  }
 
   if (postfix > 1)  {
 
@@ -292,8 +344,7 @@ function letHandleRun () {
 
 
 
-addSliderWithControl(5, 0, 100);
-addSliderWithControl(6, 0, 124);
+
 
 
 
@@ -410,7 +461,7 @@ function startValueHint (sliderHandleId) {
 
 
 
-toggleValueHint(sliders, "slider__valueCheckbox", "slider__handleLabel");
+
 
 
 
@@ -421,44 +472,77 @@ toggleValueHint(sliders, "slider__valueCheckbox", "slider__handleLabel");
 //------------- DRAG AND DROP FOR EVERY SLIDER -----------------------------
 //--------------------------------------------------------------------------
 
-
-
-
 function addHandleListener (i, slider, handle, handleLabel) {
 
   handle.onmousedown = function(event) {
     let min = sliders[i].minScaleValue,
-        max = sliders[i].maxScaleValue;
-  
+        max = sliders[i].maxScaleValue,
+        shiftX;
+
+    let isVertical = handle.classList.contains("vertical");
+
+    //---------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------
+
     event.preventDefault(); // предотвратить запуск выделения (действие браузера)
   
-    let shiftX = event.clientX - handle.getBoundingClientRect().left;
-    // shiftY здесь не нужен, слайдер двигается только по горизонтали
+    if (isVertical) {
+      shiftX = event.clientY - handle.getBoundingClientRect().top;
+    } else {
+      shiftX = event.clientX - handle.getBoundingClientRect().left;
+    }
    
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   
     function onMouseMove(event) {
-      let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-  
+      let newCoord, finishEdge;
+
+      if (isVertical) {
+        finishEdge = slider.offsetHeight - handle.offsetHeight;
+        newCoord = event.clientY - shiftX - slider.getBoundingClientRect().top;
+      } else {
+        newCoord = event.clientX - shiftX - slider.getBoundingClientRect().left;
+        finishEdge = slider.offsetWidth - handle.offsetWidth;
+      }
+
       // курсор вышел из слайдера => оставить бегунок в его границах.
-      if (newLeft < 0) {
-        newLeft = -2;
+      if (newCoord < 0) {
+        newCoord = -2;
       }
-      let rightEdge = slider.offsetWidth - handle.offsetWidth;
-      if (newLeft > rightEdge) {
-        newLeft = rightEdge;
+
+      if (newCoord > finishEdge) {
+        newCoord = finishEdge;
       }
-  
-      handle.style.left = newLeft + 'px';
+
+      if (isVertical) {
+        handle.style.top = newCoord + 'px';
+      } else {
+        handle.style.left = newCoord + 'px';
+      }
   
       //--------  расчет числа над ползунком  ---------------
       //! избавиться от ширины ползунка
-      let percent = (((handle.getBoundingClientRect().left - slider.getBoundingClientRect().left)/slider.getBoundingClientRect().width) * 100).toFixed(0);
-      if (percent > 50) {
-          percent = (((handle.getBoundingClientRect().right - slider.getBoundingClientRect().left)/slider.getBoundingClientRect().width) * 100).toFixed(0);
+
+      let handleCoordFirst, handleCoordSec, sliderCoord, sliderParam;
+
+      if (isVertical) {
+        handleCoordFirst = handle.getBoundingClientRect().top;
+        handleCoordSec = handle.getBoundingClientRect().bottom;
+        sliderCoord = slider.getBoundingClientRect().top;
+        sliderParam = slider.getBoundingClientRect().height;
+      } else {
+        handleCoordFirst = handle.getBoundingClientRect().left;
+        handleCoordSec = handle.getBoundingClientRect().right;
+        sliderCoord = slider.getBoundingClientRect().left;
+        sliderParam = slider.getBoundingClientRect().width;
       }
-      let temp = parseFloat(handle.getBoundingClientRect().width);
+ 
+      let percent = (((handleCoordFirst - sliderCoord)/sliderParam) * 100).toFixed(0);
+      if (percent > 50) {
+          percent = (((handleCoordSec - sliderCoord)/sliderParam) * 100).toFixed(0);
+      }
       
       let val = Math.round((max * percent) /100);
       handleLabel.innerHTML = val;
@@ -467,6 +551,12 @@ function addHandleListener (i, slider, handle, handleLabel) {
       let handleField = document.querySelector("#slider__handleValueField-" + idNum + "-" + postfix);
       handleField.value = val;
     };
+
+//not vertical = 
+
+    //---------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------
   
     function onMouseUp() {
       document.removeEventListener('mouseup', onMouseUp);
@@ -481,7 +571,9 @@ function addHandleListener (i, slider, handle, handleLabel) {
 };
 
 
-
+addSliderWithControl(5, 0, 100);
+addSliderWithControl(6, 0, 124);
+toggleValueHint(sliders, "slider__valueCheckbox", "slider__handleLabel");
 
 
 
